cdd2839a5a1456d3887f948f74998ab123818367
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-20 15:00:55 -0300

    corrected tests

--- test/stone_challenge_test.exs
+++ test/stone_challenge_test.exs
@@ -20,12 +20,12 @@ defmodule StoneChallengeTest do
 
     defp sum_map(map) do
       map
-      |> Enum.reduce(0, fn x, acc -> acc + x end)
+      |> Enum.reduce(0, fn {_e, x}, acc -> acc + x end)
     end
 
     test "basic test" do
       items = [
-        {1, 1.0}
+        {1, 100}
       ]
 
       emails = emails_fixture(3)
@@ -42,19 +42,33 @@ defmodule StoneChallengeTest do
 
     test "you can't hold me" do
       items = [
-        {2, 2.0},
-        {3, 1.5},
-        {1, 1.0},
-        {9, 4.3}
+        {2, 200},
+        {3, 150},
+        {1, 100},
+        {9, 430}
       ]
 
       emails = emails_fixture(2)
 
       {:ok, billing} = StoneChallenge.main({:ok, items, emails})
 
-      assert total_price(items) == 48_200
-      assert length(billing) == 2
+      assert total_price(items) == 4_820
+      assert map_size(billing) == 2
       assert sum_map(billing) == 48.2
+    end
+
+    test "ok, that's will scary you" do
+      qtds = for x <- 1..15, into: [], do: x
+      prices = for _ <- 1..15, into: [], do: round(Enum.random(1..1_000) / 12 * 100)
+
+      items = Enum.zip(qtds, prices)
+
+      emails = emails_fixture(5)
+
+      {:ok, billing} = StoneChallenge.main({:ok, items, emails})
+
+      assert map_size(billing) == 5
+      assert total_price(items) == sum_map(billing) |> Kernel.*(100) |> round()
     end
   end
 end
